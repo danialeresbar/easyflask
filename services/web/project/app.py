4903f4ed6da92572c9f143d85a97781bcfda6e7b
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-05-31 00:40:30 -0500

    Add static and template rendering feature

--- services/web/project/app.py
+++ services/web/project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 
 
 app = Flask(__name__)
@@ -8,6 +8,17 @@ app.config.from_object('project.config.Config')
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/static/<path:filename>")
+def staticfiles(filename):
+    return send_from_directory(app.config["STATIC_FOLDER"], filename)
+
+
+@app.route("/client")
+def client():
+    client_list = ['Daniel', 'Juanita', 'Francisco']
+    return render_template("client.html", clients=client_list)
 
 
 if __name__ == '__main__':
