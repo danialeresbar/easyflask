4fb59021fabbde679e43403a2b1b3494fb6fd67b
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-06-08 04:49:33 -0500

    Update manager

--- services/web/manage.py
+++ services/web/manage.py
@@ -2,17 +2,12 @@
 """Flask's command-line utility for administrative tasks."""
 from flask.cli import FlaskGroup
 
-from app import app, db
+from app import create_app
+# from app.auth.models import *
+# from app.bookstore.models import *
 
 
-cli = FlaskGroup(app)
-
-
-@cli.command('create_database')
-def create_database():
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
+cli = FlaskGroup(create_app())
 
 
 if __name__ == '__main__':
