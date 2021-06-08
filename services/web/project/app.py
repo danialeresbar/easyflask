4101128de4b9f6cf7497f0c6ec705971de22013e
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-06-07 22:41:52 -0500

    Add ORM feature

--- services/web/project/app.py
+++ services/web/project/app.py
@@ -1,19 +1,31 @@
 import os
-from flask import Flask, render_template, request, send_from_directory
 
+from flask import Flask, render_template, request, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+
+
+csrf = CSRFProtect()
 
 app = Flask(__name__)
 environment = os.environ.get('FLASK_ENV', 'development')
 app.config.from_object('project.settings.{}'.format(environment))
+csrf.init_app(app)
+database = SQLAlchemy(app)
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('auth/login.html')
 
 
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found():
     return render_template('errors/404.html')
 
 
