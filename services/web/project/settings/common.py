a26db3fccdb87451800e6b107e37d13cfca52c70
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-06-07 22:39:35 -0500

    Add database settings

--- services/web/project/settings/common.py
+++ services/web/project/settings/common.py
@@ -2,14 +2,12 @@ import os
 from pathlib import Path
 
 
-# Environment variable
-ENVIRONMENT = os.environ.get('ENVIRONMENT', 'development')
+# Environment
+ENVIRONMENT = os.environ.get('FLASK_ENV', 'development')
 IS_PRODUCTION = ENVIRONMENT == "production"
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 PROJECT_DIR = Path(__file__).resolve().parent.parent
-
 
 # Static files (CSS, JavaScript, Images)
 STATIC_FOLDER = f"{os.getenv('USER_HOME', '')}/app/project/static"
--- services/web/project/settings/development.py
+++ services/web/project/settings/development.py
@@ -2,3 +2,21 @@ from .common import *
 
 
 DEBUG = True
+SECRET_KEY = "flaskn4%(+ftkg6-r3jl585^542-4&i(f8n*iw(p__v^o8xc^m@2qlckey"
+
+# Database
+database_settings = {
+    'NAME': 'easyflask',
+    'USER': 'flaskuser',
+    'PASSWORD': 'mariadb',
+    'HOST': 'database',
+    'PORT': '3306'
+}
+SQLALCHEMY_DATABASE_URI = "mysql://{}:{}@{}:{}/{}".format(
+    database_settings.get('USER'),
+    database_settings.get('PASSWORD'),
+    database_settings.get('HOST'),
+    database_settings.get('PORT'),
+    database_settings.get('NAME')
+)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
