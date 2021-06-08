3e1460c43018adb5c035aa0827d976cc58ef55ce
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-06-08 04:29:19 -0500

    Update database connector

--- services/web/app/settings/development.py
+++ services/web/app/settings/development.py
@@ -10,11 +10,11 @@ SECRET_KEY = "flaskn4%(+ftkg6-r3jl585^542-4&i(f8n*iw(p__v^o8xc^m@2qlckey"
 database_settings = {
     'NAME': 'easyflask',
     'USER': 'flaskuser',
-    'PASSWORD': 'mariadb',
+    'PASSWORD': 'mysql',
     'HOST': 'database',
     'PORT': '3306'
 }
-SQLALCHEMY_DATABASE_URI = "mysql://{}:{}@{}:{}/{}".format(
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{}:{}@{}:{}/{}".format(
     database_settings.get('USER'),
     database_settings.get('PASSWORD'),
     database_settings.get('HOST'),
